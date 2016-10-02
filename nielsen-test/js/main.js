7da1e17a612b9cb5b33a32487116b4f1341b8aba
Edward <ultra.edward@gmail.com>
2016-10-02 13:12:41 -0700

    nielsen progress
    - style updates
    - attempting to lowercase all output text
    - removal of last output comma

--- nielsen-test/js/main.js
+++ nielsen-test/js/main.js
@@ -1,14 +1,12 @@
 // original provided array with duplicate names
-var duplicatesArray = ['Nick', 'jake', 'RAY', 'Kate', 'Nick', 'Jeremy', 'nick', 'AMOL', 'rAY', 'VIANNEY', 'Shilpika', 'nick', 'THOMAS', 'tom', 'james', 'JERM', 'amOl', 'kate', 'SCOTT', 'Jenifer', 'bill', 'jenny', 'STEVEN'];
+var duplicatesArray = ['Nick', 'jake', 'RAY', 'Kate', 'Nick', 'Jeremy', 'nick', 'AMOL', 'rAY', 'VIANNEY', 'Shilpika', 'nick', 'THOMAS', 'tom', 'james', 'JERM', 'amOl', 'kate', 'SCOTT', 'Jenifer', 'bill', 'jenny', 'STEVEN']
+var duplicatesArrayToString = duplicatesArray.toString()
+var duplicatesArrayLowerCase = duplicatesArrayToString.toLowerCase()
 
 // remove duplicates and position elements
 var uniqueArray = duplicatesArray.filter(function(elem, pos) {
 return duplicatesArray.indexOf(elem) == pos
 })
-
-uniqueArray.toString(console.log('string'))
-
-var noCommaArray = uniqueArray.replace(/,\s*$/, '')
 
 // onclick button
 	$('#click').on('click',function(){
@@ -17,14 +15,14 @@ var noCommaArray = uniqueArray.replace(/,\s*$/, '')
 	$('.unique').append('[')
 		
 		// for loop through each item of uniqueArray, append to unique div
-		for (var i=0; i<noCommaArray.length;i++){
-			$('.unique').append("'" + noCommaArray[i] +"', ")
+		for (var i=0; i<duplicatesArrayLowerCase.length;i++){
+			$('.unique').append("'" + duplicatesArrayLowerCase[i] +"', ")
 			}
 
 	// append marking for reference 
 	$('.unique').append(']')
 
 	// print filtered unique array to console
-	console.log(noCommaArray)
+	console.log(duplicatesArrayLowerCase)
 	})
 // end onclick button
